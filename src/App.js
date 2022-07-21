f1ab66d490d25e37ea58e80d4f2f1042207ff7d5
Victor Volovik <VictorVolovik@users.noreply.github.com>
2022-07-21 18:53:03 +0300

    add counter example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { atom, useAtom } from "jotai";
+
+const countAtom = atom(0);
+
+const Count = ({ className }) => {
+  const [count] = useAtom(countAtom);
+
+  return <h2 className={className}>Count: {count}</h2>;
+};
+
+const CountDecrease = () => {
+  const [, setCount] = useAtom(countAtom);
+
+  return <button onClick={() => setCount((c) => c - 1)}>-</button>;
+};
+
+const CountIncrease = () => {
+  const [, setCount] = useAtom(countAtom);
+
+  return <button onClick={() => setCount((c) => c + 1)}>+</button>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* counter example */}
+      <div className="Counter">
+        <CountDecrease />
+        <Count className="Count" />
+        <CountIncrease />
+      </div>
     </div>
   );
 }
